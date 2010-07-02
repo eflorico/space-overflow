8147ba416004fd6a946d0be7a6787f6dfe945ebb
eric.wolf123 <eric.wolf123@7168c144-cbbf-a26d-18e6-bd0a78aa1c8c>
2010-07-02 17:08:10 +0000

    Added Effects documentation.

--- trunk/SpaceOverflow/SpaceOverflow/Effects/PropertyOrFieldDescriptor.cs
+++ trunk/SpaceOverflow/SpaceOverflow/Effects/PropertyOrFieldDescriptor.cs
@@ -6,6 +6,9 @@ using System.Reflection;
 
 namespace SpaceOverflow.Effects
 {
+    /// <summary>
+    /// Describes a property or field based on a MemberInfo.
+    /// </summary>
     public class PropertyOrFieldDescriptor 
     {
         public PropertyOrFieldDescriptor(MemberInfo member) {
@@ -14,17 +17,26 @@ namespace SpaceOverflow.Effects
 
         public MemberInfo Member { get; private set; }
 
+        /// <summary>
+        /// Sets the value of the property/field on the specified owner object.
+        /// </summary>
         public void SetValue(object owner, object value) {
             if (this.Member is PropertyInfo) ((PropertyInfo)this.Member).SetValue(owner, value, new object[0]);
             else ((FieldInfo)this.Member).SetValue(owner, value);
-
         }
 
+        /// <summary>
+        /// Gets the value of the property/field on the specified owner object.
+        /// </summary>
+        /// <returns></returns>
         public object GetValue(object owner) {
             if (this.Member is PropertyInfo) return ((PropertyInfo)this.Member).GetValue(owner, new object[0]);
             else return ((FieldInfo)this.Member).GetValue(owner);
         }
 
+        /// <summary>
+        /// The return type of the property/field.
+        /// </summary>
         public Type Type {
             get {
                 if (this.Member is PropertyInfo) return ((PropertyInfo)this.Member).PropertyType;
