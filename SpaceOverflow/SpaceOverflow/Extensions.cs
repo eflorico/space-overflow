10d5de2ae0fa042809221dd111095ba3a4547930
eric.wolf123 <eric.wolf123@7168c144-cbbf-a26d-18e6-bd0a78aa1c8c>
2010-07-13 16:04:45 +0000

    - Cleanup (removed unneeded functions)

--- SpaceOverflow/SpaceOverflow/Extensions.cs
+++ SpaceOverflow/SpaceOverflow/Extensions.cs
@@ -5,6 +5,8 @@ using System.Text;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Input;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace SpaceOverflow
 {
@@ -12,78 +14,6 @@ namespace SpaceOverflow
     {
         public static void Draw(this SpriteBatch spriteBatch, Texture2D texture, Vector2 position, Color color, SpriteEffects effects) {
             spriteBatch.Draw(texture, position, null, color, 0f, new Vector2(), 1f, effects, 0);
-        }
-
-        public static void DrawTiled(this SpriteBatch spriteBatch, Texture2D texture, Rectangle target, Color color) {
-            spriteBatch.DrawTiled(texture, target, color, SpriteEffects.None);
-        }
-
-        public static void DrawTiled(this SpriteBatch spriteBatch, Texture2D texture, Rectangle target, Color color, SpriteEffects effects) {
-            //spriteBatch.DrawTiled(texture, target, new Rectangle(0, 0, texture.Width, texture.Height), color, effects);
-            spriteBatch.Draw(texture, target, null, color, 0f, new Vector2(), effects, 0);
-
-            return; //TODO: Fix or drop
-            int targetX = target.X, targetY = target.Y,
-               sourceX = 0, sourceY = 0;
-
-            do {
-                do {
-                    var width = Math.Min(target.Right - targetX, texture.Width - sourceX);
-                    var height = Math.Min(target.Bottom - targetY, texture.Height - sourceY);
-
-                    spriteBatch.Draw(texture,
-                        new Rectangle(targetX, targetY, width, height),
-                        new Rectangle(sourceX, sourceY, width, height),
-                        color,
-                        0f,
-                        new Vector2(),
-                        effects,
-                        0);
-
-                    targetX += width;
-                    targetY += height;
-
-                    sourceX += width;
-                    sourceY += height;
-
-                    if (sourceX == texture.Width) sourceX = 0;
-                    if (sourceY == texture.Height) sourceY = 0;
-                }
-                while (targetY < target.Bottom);
-            }
-            while (targetX < target.Right);
-        }
-
-        public static void DrawTiled(this SpriteBatch spriteBatch, Texture2D texture, Rectangle target, Rectangle source, Color color, SpriteEffects effects) {
-            int targetX = target.X, targetY = target.Y, 
-                sourceX = source.X % texture.Width, sourceY = source.Y % texture.Height;
-
-            do {
-                do {
-                    var width = Math.Min(target.Right - targetX, source.Right - sourceX);
-                    var height = Math.Min(target.Bottom - targetY, source.Bottom - sourceY);
-
-                    spriteBatch.Draw(texture,
-                        new Rectangle(targetX, targetY, width, height),
-                        new Rectangle(sourceX, sourceY, width, height),
-                        color,
-                        0f,
-                        new Vector2(),
-                        effects,
-                        0);
-
-                    targetX += width;
-                    targetY += height;
-
-                    sourceX += width;
-                    sourceY += height;
-
-                    if (sourceX == source.Right) sourceX = source.X;
-                    if (sourceY == source.Bottom) sourceY = source.Y;
-                }
-                while (targetY < target.Bottom);
-            }
-            while (targetX < target.Right);
         }
 
         public static void DrawString(this SpriteBatch spriteBatch, SpriteFont font, string text, Vector2 position, TextAlignment alignment, Color color) {
@@ -100,16 +30,6 @@ namespace SpaceOverflow
             for (var i = 0; i < data.Length; ++i)
                 data[i] = color;
             texture.SetData(data);
-        }
-
-        public static float ExponentialInterpolate(float value1, float value2, float amount) {
-            value2 -= value1;
-            amount -= value1;
-
-            var final = (float)Math.Sqrt(value2);
-            var pos = amount * final;
-
-            return pos * pos;
         }
 
         public static Vector2 GetPosition(this MouseState mouseState) {
